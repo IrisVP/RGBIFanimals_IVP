f3467bacc399b657851ae13b8793f12c2050c8bd
TimBosman <50997599+TimBosman@users.noreply.github.com>
2023-09-13 21:02:37 +0200

    test occurrence data

--- 6_Tests.R
+++ 6_Tests.R
@@ -3,20 +3,17 @@ source("2_Functions.R")
 library("testthat")
 
 test_that("Create rastered world", {
-  expect_true(TRUE)
   expect_no_error(create_rastered_world("Inputs/tr.rdata"))
+  # TODO: add more tests here
 })
 
-test_that("get_occurrence_data", {
-  expect_true(TRUE)
+test_that("Get occurrence data", {
+  species <- "Acartia margalefi"
+  expect_no_error(test <- get_occurrence_data(species))
+  expect_gte(nrow(test), 6)
 })
-
 
 test_that("check_occurrence_data", {
-  expect_true(TRUE)
-})
-
-test_that("plot_distribution", {
   expect_true(TRUE)
 })
 
@@ -29,10 +26,6 @@ test_that("filter_n_closest_coordinate_ceiling", {
 })
 
 test_that("find_shortest_route_in_sea", {
-  expect_true(TRUE)
-})
-
-test_that("plot_shortest_path", {
   expect_true(TRUE)
 })
 
