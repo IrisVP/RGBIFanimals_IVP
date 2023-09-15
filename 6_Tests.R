f41ad47fb3337e04fec9884cdb6bd33a2b657f63
TimBosman <50997599+TimBosman@users.noreply.github.com>
2023-09-15 14:46:30 +0200

    Add failing test to new test directory

--- 6_Tests.R
+++ 6_Tests.R
@@ -35,8 +35,8 @@ test_that("find_closest_registered_place", {
                             "Longitude" = 2.79178508,
                             "Latitude" = 51.6533567)
   tr <- create_rastered_world("Inputs/tr.rdata")
-  # expect_no_condition(find_closest_registered_place(species, coordinates,
-  #                                                   tr, "test_outputs/tests.csv"))
+  expect_no_condition(find_closest_registered_place(species, coordinates,
+                                                    tr, "test_outputs/tests.csv"))
   # TODO: add more tests here
   expect_true(TRUE)
 })
