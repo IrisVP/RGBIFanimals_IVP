22a3757fda35793af061da4ab71e9575971ac20e
TimBosman <50997599+TimBosman@users.noreply.github.com>
2023-09-15 12:14:40 +0200

    Add new tests

--- 6_Tests.R
+++ 6_Tests.R
@@ -14,13 +14,26 @@ test_that("Get occurrence data", {
 })
 
 test_that("check_occurrence_data", {
+  # test file that is available
   species <- "Acartia margalefi"
+  filename <- paste0("OccurrenceData/", species, ".csv")
+  expect_true(file.exists(filename))
   expect_no_error(test <- check_occurrence_data(species))
   expect_gte(nrow(test), 6)
+  # test animal not existing in the data set
+  species <- "Desulfurococcus amylolyticus"
+  filename <- paste0("OccurrenceData/", species, ".csv")
+  expect_false(file.exists(filename))
+  expect_no_error(test <- check_occurrence_data(species))
+  expect_true(file.exists(filename))
+  expect_gte(nrow(test), 4)
 })
 
 test_that("find_closest_registered_place", {
-  expect_true(TRUE)
+  species <- "Acartia margalefi"
+  coordinates <- t(data.frame(row.names = c("Observatory.ID","Longitude","Latitude"), "1" = c("BelgianCoast",2.79178508,51.6533567)))
+  tr <- create_rastered_world("Inputs/tr.rdata")
+  expect_no_condition(find_closest_registered_place(species, coordinates, tr, "Output/tests.csv"))
   # TODO: add more tests here
 })
 
@@ -30,8 +43,19 @@ test_that("filter_n_closest_coordinate_ceiling", {
 })
 
 test_that("find_shortest_route_in_sea", {
-  expect_true(TRUE)
-  # TODO: add more tests here
+  row <- data.frame("species" = "Acartia margalefi",
+                    "location" = "Roscoff",
+                    "value" = 1570)
+  samplelocation <- data.frame("location" = "Roscoff",
+                               "Longitude" = -3.9665,
+                               "Latitude" = 48.7175)
+  occurrence_data <- check_occurrence_data(row[1])
+  tr <- create_rastered_world("Inputs/tr.rdata")
+  filename <- "Output/DistanceOverSeaFORTESTS.csv"
+  expect_false(file.exists(filename))
+  expect_no_error(find_shortest_route_in_sea(samplelocation, occurrence_data, tr, row, filename))
+  expect_true(file.exists(filename))
+  TODO: add more tests here
 })
 
 test_that("filter_on_distance", {
