23f8f55b2890dbc5387e6e46498ba1a729456767
Iris van paemel <iris.van.paemel@student.howest.be>
2024-05-30 17:32:35 +0200

    changed graphs layout and details in 4.2_Visualisation_distribution.R script

--- 4.2_Visualisation_distribution.R
+++ 4.2_Visualisation_distribution.R
@@ -19,8 +19,6 @@ long <- long[long$value > 0, ]
 
 # iterate over species_location file again using function and map()
 # and read csv file per species
-species_name <- "Acartia margalefi"     # organism that has few occurrences??
-species_location <- "SwedishWestCoast"
 
 ########################## FUNCTION
 Distribution_seadistance <- function(species_name, species_location){
@@ -29,52 +27,46 @@ Distribution_seadistance <- function(species_name, species_location){
   print(paste0("species_location: ", species_location))
   distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
   
-  distances <- read.table(distance_file)
+  distances <- read.table(distance_file, header = TRUE)
   distances$x <- distances$x/1000
+  #print(summary(distances))
+  distances <- subset(distances, x < 40000)
+  #print(summary(distances))
   
   ###############################################################################
-  # make histograms of distances per species, without filtering on distance limit
+  # make histograms of distances per species, with filtering on distance limit 40000
   ###############################################################################
   
   dist_plot <- ggplot(distances, aes(x = x)) +
-    geom_histogram(binwidth = 200, fill = "blue", color = "black") +
-    labs(title = "Histogram of Distances", x = "Distance", y = "Frequency") +
-    theme_minimal() +
+    geom_histogram(binwidth = 200, fill = "blue", color = "black", boundary = 0) +
+    labs(title = "Histogram of Distances", x = "Distance in km", y = "Frequency of species") +
+    theme_bw() +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
           plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
-          axis.text = element_text(size = 17),           # Set font size for axis numbers
-          axis.title = element_text(size = 20),          # Set font size for axis titles
-          panel.background = element_rect(fill = "white", color = NA),
-          plot.background = element_rect(fill = "white", color = NA),) +
-    ylab("Frequency of species") +
-    xlab("Distance in km")
+          axis.text = element_text(size = 16),           # Set font size for axis numbers
+          axis.title = element_text(size = 20)) +         # Set font size for axis titles
+    scale_x_continuous(breaks = seq(0, 40500, by = 1500), expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = c(0, 40500)) # Use coord_cartesian for setting limits
     
   #print(dist_plot)
   # create a directory to save the plots in
   
-  if(!dir.exists("test_outputs/distribution_plots")) {
-    dir.create("test_outputs/distribution_plots")
-    if (!exists(paste0("test_outputs/distribution_plots/plot_", species_name, "_from_", species_location, ".png"))) {
-      ggsave(filename = paste0("test_outputs/distribution_plots/plot_", species_name, "_from_", species_location, ".png"), 
-             plot = dist_plot, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " sea distance plot already created"))
-    }
+  if(!dir.exists("test_outputs/sea_distribution_plots")) {
+    dir.create("test_outputs/sea_distribution_plots")
+    ggsave(filename = paste0("test_outputs/sea_distribution_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = dist_plot, width = 50, height = 30, units = "cm", dpi = 900)
   } else {
-    if (!exists(paste0("test_outputs/distribution_plots/plot_", species_name, "_from_", species_location, ".png"))) {
-      ggsave(filename = paste0("test_outputs/distribution_plots/plot_", species_name, "_from_", species_location, ".png"), 
-             plot = dist_plot, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " sea distance plot already created"))
-    }
+    ggsave(filename = paste0("test_outputs/sea_distribution_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = dist_plot, width = 50, height = 30, units = "cm", dpi = 900)
   }
   
   ##################################################################
   # make histograms for species but with a limit set to 10000km away
   ##################################################################
   lim <- 10000
-  distances_subset <- as.data.frame(subset(distances, x < lim))  # take distances less than 10000 km away
+  distances_subset <- subset(distances, x < lim)  # take distances less than 10000 km away
   
   # check if there are datapoints in this subset
   if(nrow(distances_subset) == 0) {
@@ -86,36 +78,25 @@ Distribution_seadistance <- function(species_name, species_location){
   # make histograms of distances per species, filtering on distance limit
   dist_plot_10000 <- ggplot(distances_subset, aes(x = x)) +
     geom_histogram(binwidth = 200, fill = "blue", color = "black") +
-    labs(x = "Distance", y = "Frequency") +
-    theme_minimal() +
+    labs(x = "Distance in km", y = "Frequency of species") +
+    theme_bw() +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
           plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
-          axis.text = element_text(size = 17),           # Set font size for axis numbers
-          axis.title = element_text(size = 20),          # Set font size for axis titles
-          panel.background = element_rect(fill = "white", color = NA),
-          plot.background = element_rect(fill = "white", color = NA),) +
-    ylab("Frequency of species") +
-    xlab("Distance in km")
+          axis.text = element_text(size = 16),           # Set font size for axis numbers
+          axis.title = element_text(size = 20)) +        # Set font size for axis titles
+    scale_x_continuous(breaks = seq(0, lim, by = 1500), limits = c(0, lim))
 
   #print(dist_plot_10000)
   # create a directory to save the plots in
   
-  if(!dir.exists(paste0("test_outputs/distribution_limit", lim, "_plots"))) {
-    dir.create(paste0("test_outputs/distribution_limit", lim, "_plots"))
-    if (!exists(paste0("test_outputs/distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"))) {
-      ggsave(filename = paste0("test_outputs/distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"), 
-              plot = dist_plot_10000, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " sea distance plot with limit already created"))
-    }
+  if(!dir.exists(paste0("test_outputs/sea_distribution_limit", lim, "_plots"))) {
+    dir.create(paste0("test_outputs/sea_distribution_limit", lim, "_plots"))
+    ggsave(filename = paste0("test_outputs/sea_distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"), 
+            plot = dist_plot_10000, width = 50, height = 30, units = "cm", dpi = 900)
   } else {
-    if (!exists(paste0("test_outputs/distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"))) {
-      ggsave(filename = paste0("test_outputs/distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"), 
-            plot = dist_plot_10000, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " sea distance plot with limit already created"))
-    }
+    ggsave(filename = paste0("test_outputs/sea_distribution_limit", lim, "_plots/plot_", species_name, "_from_", species_location, lim, ".png"), 
+          plot = dist_plot_10000, width = 50, height = 30, units = "cm", dpi = 900)
   }
 }  
 ################################# END FUNCTION
@@ -139,8 +120,6 @@ if(!exists("test_outputs/errors_lessthan", lim)) {
 #############################################################
 # Make combined histogram of sea distances and fly distances
 #############################################################
-species_name <- "Acartia bifilosa"     # organism that has few occurrences??
-species_location <- "Roscoff"
 
 Distribution_combDistance <- function(species_name, species_location){
   sea_distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
@@ -149,8 +128,10 @@ Distribution_combDistance <- function(species_name, species_location){
   sea_distances <- read.table(sea_distance_file)
   fly_distances <- read.table(fly_distance_file)
   sea_distances$x <- sea_distances$x/1000
+  sea_distances <- subset(sea_distances, x < 40000)
   sea_distances$type <- "sea_distance"
   fly_distances$x <- fly_distances$x/1000
+  fly_distances <- subset(fly_distances, x < 40000)
   fly_distances$type <- "fly_distance"
   
   combined_distances <- rbind(sea_distances, fly_distances)
@@ -158,35 +139,34 @@ Distribution_combDistance <- function(species_name, species_location){
   p <- combined_distances %>%
     ggplot( aes(x=x, fill=type)) +
     geom_histogram(binwidth = 200, color="#e9ecef", alpha=0.6, position = 'identity') +
-    scale_fill_manual(values=c("#69b3a2", "#404080")) +
     theme_bw() +
-    labs(x = "Distance", y = "Frequency") +
+    labs(x = "Distance in km", y = "Frequency") +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(
       plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
       plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
-      axis.text = element_text(size = 12),  # Set font size for axis numbers
+      axis.text = element_text(size = 16),  # Set font size for axis numbers
       axis.title = element_text(size = 20),
-      ) +
-    scale_x_continuous(breaks = seq(0, max(combined_distances$x), by = 2000)) # Add extra values to the x-axis
-  print(p)
+      legend.title = element_text(size = 18, face="bold"),
+      legend.text = element_text(size = 16)) +
+    scale_x_continuous(breaks = seq(0, 40500, by = 1500), expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = c(0, 40500)) + # Use coord_cartesian for setting limits
+    # set legend title and labels
+    scale_fill_discrete(
+      name = "Distance type",
+      breaks = c("sea_distance", "fly_distance"),
+      labels = c("Sea distance", "Fly distance")) +
+    labs(x = "Distance in km", y = "Frequency")
   
-  #print(p)
+  
   if(!dir.exists(paste0("test_outputs/combined_distribution_plots"))) {
     dir.create(paste0("test_outputs/combined_distribution_plots"))
-    if (!exists(paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"))) {
-      ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
-             plot = p, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " combined distance plot already created"))
-    }
+    ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
+           plot = p, width = 50, height = 30, units = "cm", dpi = 900)
   } else {
-    if (!exists(paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"))) {
-      ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
-             plot = p, width = 40, height = 30, units = "cm", dpi = 900)
-    } else {
-      warning(paste0(species_name, " for ", species_location, " combined distance plot already created"))
-    }
+    ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
+           plot = p, width = 50, height = 30, units = "cm", dpi = 900)
   }
 }
 
