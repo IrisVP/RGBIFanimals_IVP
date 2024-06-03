28a122da6cd14de61c22e2439d4338537e375da1
Iris van paemel <iris.van.paemel@student.howest.be>
2024-06-03 21:06:19 +0200

    made new cregional graphs and adjusted details to all the graphs

--- new_workflow_Iris/4.2_Visualisation_distribution.R
+++ new_workflow_Iris/4.2_Visualisation_distribution.R
@@ -4,6 +4,7 @@
 library("tidyr")
 library("ggplot2")
 library("dplyr")
+library("poliscidata")
 ####################################################
 
 ####################################################
@@ -11,16 +12,24 @@ library("dplyr")
 # Set working directory to directory where the R-script is saved
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # first load in species_location file
-species_location <- read.csv("Output/RealFirst10_Species_Location.csv")
+species_location <- read.csv("Output/next20_Species_Location.csv")
 long <- pivot_longer(species_location, !Specieslist)
 # from tidyr package, reshape df from wide to long format
 long <- long[long$value > 0, ]
+
+
+
+
 ####################################################
 
 # iterate over species_location file again using function and map()
 # and read csv file per species
 
 ########################## FUNCTION
+
+species_name <- "Aleochara obscurella"
+species_location <- "Getxo"
+
 Distribution_seadistance <- function(species_name, species_location){
   # read csv file per species
   print(paste0("species_name: ", species_name))
@@ -28,19 +37,20 @@ Distribution_seadistance <- function(species_name, species_location){
   distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
   
   distances <- read.table(distance_file, header = TRUE)
-  distances$x <- distances$x/1000
+  distances$sea_distances <- distances$sea_distances/1000
   #print(summary(distances))
-  distances <- subset(distances, x < 40000)
+  distances <- subset(distances, sea_distances < 40000)
   #print(summary(distances))
   
   ###############################################################################
   # make histograms of distances per species, with filtering on distance limit 40000
   ###############################################################################
   
-  dist_plot <- ggplot(distances, aes(x = x)) +
+  dist_plot <- ggplot(distances, aes(x = sea_distances)) +
     geom_histogram(binwidth = 200, fill = "blue", color = "black", boundary = 0) +
     labs(title = "Histogram of Distances", x = "Distance in km", y = "Frequency of species") +
     theme_bw() +
+    scale_fill_brewer(palette = "Set1") +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
           plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
@@ -66,7 +76,7 @@ Distribution_seadistance <- function(species_name, species_location){
   # make histograms for species but with a limit set to 10000km away
   ##################################################################
   lim <- 10000
-  distances_subset <- subset(distances, x < lim)  # take distances less than 10000 km away
+  distances_subset <- subset(distances, sea_distances < lim)  # take distances less than 10000 km away
   
   # check if there are datapoints in this subset
   if(nrow(distances_subset) == 0) {
@@ -76,10 +86,11 @@ Distribution_seadistance <- function(species_name, species_location){
   }
   
   # make histograms of distances per species, filtering on distance limit
-  dist_plot_10000 <- ggplot(distances_subset, aes(x = x)) +
+  dist_plot_10000 <- ggplot(distances_subset, aes(x = sea_distances)) +
     geom_histogram(binwidth = 200, fill = "blue", color = "black") +
     labs(x = "Distance in km", y = "Frequency of species") +
     theme_bw() +
+    scale_fill_brewer(palette = "Set1") +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
           plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
@@ -120,6 +131,8 @@ if(!exists("test_outputs/errors_lessthan", lim)) {
 #############################################################
 # Make combined histogram of sea distances and fly distances
 #############################################################
+species_name <- "Acartia bifilosa"
+species_location <- "Koster"
 
 Distribution_combDistance <- function(species_name, species_location){
   sea_distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
@@ -127,11 +140,11 @@ Distribution_combDistance <- function(species_name, species_location){
   
   sea_distances <- read.table(sea_distance_file)
   fly_distances <- read.table(fly_distance_file)
-  sea_distances$x <- sea_distances$x/1000
-  sea_distances <- subset(sea_distances, x < 40000)
+  sea_distances$sea_distances <- sea_distances$sea_distances/1000
+  sea_distances <- subset(sea_distances, sea_distances < 40000)
   sea_distances$type <- "sea_distance"
-  fly_distances$x <- fly_distances$x/1000
-  fly_distances <- subset(fly_distances, x < 40000)
+  fly_distances$fly_distances <- fly_distances$fly_distances/1000
+  fly_distances <- subset(fly_distances, fly_distances < 40000)
   fly_distances$type <- "fly_distance"
   
   combined_distances <- rbind(sea_distances, fly_distances)
@@ -140,6 +153,7 @@ Distribution_combDistance <- function(species_name, species_location){
     ggplot( aes(x=x, fill=type)) +
     geom_histogram(binwidth = 200, color="#e9ecef", alpha=0.6, position = 'identity') +
     theme_bw() +
+    scale_fill_brewer(palette = "Set1") +
     labs(x = "Distance in km", y = "Frequency") +
     ggtitle(paste0("Distribution of ", species_name, " from ", species_location)) +
     theme(
@@ -163,14 +177,156 @@ Distribution_combDistance <- function(species_name, species_location){
   if(!dir.exists(paste0("test_outputs/combined_distribution_plots"))) {
     dir.create(paste0("test_outputs/combined_distribution_plots"))
     ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
-           plot = p, width = 50, height = 30, units = "cm", dpi = 900)
+           plot = p, width = 60, height = 30, units = "cm", dpi = 900)
   } else {
     ggsave(filename = paste0("test_outputs/combined_distribution_plots/plotcomb_", species_name, "_from_", species_location,".png"), 
-           plot = p, width = 50, height = 30, units = "cm", dpi = 900)
+           plot = p, width = 60, height = 30, units = "cm", dpi = 900)
   }
 }
 
 # executing iteration for the long file!
 plot <- Map(Distribution_combDistance, long$Specieslist, long$name)
 
+#############################################################
+# Make histograms of locations
+#############################################################
+species_name <- "Ancula gibbosa"
+# Alcyonium digitatum
+species_location <- "Vigo"
+
+Location_histograms <- function(species_name, species_location){
+  # read csv file per species
+  print(paste0("species_name: ", species_name))
+  print(paste0("species_location: ", species_location))
+  distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
+  
+  distance_file <- read.table(distance_file, header = TRUE)
+  distance_file$x <- distance_file$x/1000
+  
+  distances <- subset(distance_file, x < 40000)
+  countries <- distances$country
+  #############################################################
+  
+  data(world)
+  # Select relevant columns from the world dataset and rename them for clarity
+  region_mapping <- world %>%
+    filter(country %in% countries) %>%
+    dplyr::select(Country = country, Region = regionun)
+  
+  # Join your data with the region mapping
+  # hard-coding some country names from GBIF that are not linked to a region in data(world)
+  df_with_regions <- distances %>%
+    left_join(region_mapping, by = c("country" = "Country")) %>%
+    mutate(Region = if_else(country == "United States of America", "North America", Region)) %>%
+    mutate(Region = if_else(country == "Russian Federation", "Russian Federation", Region)) %>%
+    mutate(Region = if_else(country == "Tonga", "Australia/New Zealand/Oceania", Region)) %>%
+    mutate(Region = if_else(country == "Svalbard and Jan Mayen", "Svalbard", Region)) %>%
+    mutate(Region = if_else(country == "Korea, Republic of", "Asia", Region)) %>%
+    mutate(Region = if_else(country == "United Kingdom of Great Britain and Northern Ireland", "Europe", Region))
+  
+  
+  ### PLOT
+  
+  country_plot <- ggplot(df_with_regions, aes(x = x, fill = Region)) +
+    geom_histogram(binwidth = 500, boundary = 0, position = "stack") +  # adjust the binwidth to personal preference
+    labs(title = paste0("Frequencies of Sea distances/country for ", species_name," in ", species_location),
+                        x = "Sea distance in km", y = "Frequency of species") +
+    theme_bw() +
+    scale_fill_brewer(palette = "Set1") +  # You can choose a different palette if you like
+    theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
+          plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
+          axis.text = element_text(size = 16),           # Set font size for axis numbers
+          axis.title = element_text(size = 20),
+          legend.title = element_text(size = 14),   # Increase legend title size
+          legend.text = element_text(size = 12),    # Increase legend text size
+          legend.key.size = unit(1.5, "lines")) +   # Increase legend key size
+    scale_x_continuous(breaks = seq(0, 40500, by = 1500), expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = c(0, 40500)) # Use coord_cartesian for setting limits
+  
+  print(country_plot)
+  # create a directory to save the plots in
+  
+  if(!dir.exists("test_outputs/sea_distribution_country_plots")) {
+    dir.create("test_outputs/sea_distribution_country_plots")
+    ggsave(filename = paste0("test_outputs/sea_distribution_country_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = country_plot, width = 60, height = 30, units = "cm", dpi = 900)
+  } else {
+    ggsave(filename = paste0("test_outputs/sea_distribution_country_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = country_plot, width = 60, height = 30, units = "cm", dpi = 900)
+  }
+}
+
+# executing iteration for the long file!
+plot <- Map(Location_histograms, long$Specieslist, long$name)
+
+
+
+#############################################################
+# Make histograms of year categories
+#############################################################
+#species_name <- "Alcyonium digitatum"
+# Alcyonium digitatum
+#species_location <- "Koster"
+
+Year_histograms <- function(species_name, species_location){
+  # read csv file per species
+  print(paste0("species_name: ", species_name))
+  print(paste0("species_location: ", species_location))
+  distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
+  
+  distance_file <- read.table(distance_file, header = TRUE)
+  distance_file$sea_distances <- distance_file$sea_distances/1000
+  
+  distances <- subset(distance_file, sea_distances < 40000)
+  years <- distances$year
+  #############################################################
+  assign_category <- function(year) {
+    for (category in year_categories) {
+      range <- as.numeric(unlist(strsplit(category, "-")))
+      if (year >= range[1] & year <= range[2]) {
+        return(category)
+      }
+    }
+    return(NA) # If year doesn't fall into any category
+  }
+  year_categories <- c("1990-1995","1995-2000","2000-2005","2005-2010","2010-2015","2015-2020","2020-2025")
+  # Apply function to create new column
+  distances$year_category <- sapply(distances$year, assign_category)
+  
+  #################################
+  ### PLOT
+  
+  year_plot <- ggplot(df_with_regions, aes(x = sea_distances, fill = distances$year_category)) +
+    geom_histogram(binwidth = 500, boundary = 0, position = "stack") +  # adjust the binwidth to personal preference
+    labs(title = paste0("Frequencies of Sea distances/year for ", species_name," in ", species_location),
+         x = "Sea distance in km", y = "Frequency of species") +
+    theme_bw() +
+    scale_fill_brewer(palette = "Set1") + # You can choose a different palette if you like
+    theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"), # set title font size, placement
+          plot.margin = margin(0.3, 0.3, 0.4, 0.4, "cm"),
+          axis.text = element_text(size = 16),           # Set font size for axis numbers
+          axis.title = element_text(size = 20),
+          legend.title = element_text(size = 14),   # Increase legend title size
+          legend.text = element_text(size = 12),    # Increase legend text size
+          legend.key.size = unit(1.5, "lines")) +   # Increase legend key size
+    scale_x_continuous(breaks = seq(0, 40500, by = 1500), expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    coord_cartesian(xlim = c(0, 40500)) # Use coord_cartesian for setting limits
+  
+  print(year_plot)
+  # create a directory to save the plots in
+  
+  if(!dir.exists("test_outputs/sea_distribution_year_plots")) {
+    dir.create("test_outputs/sea_distribution_year_plots")
+    ggsave(filename = paste0("test_outputs/sea_distribution_year_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = year_plot, width = 60, height = 30, units = "cm", dpi = 900)
+  } else {
+    ggsave(filename = paste0("test_outputs/sea_distribution_year_plots/plot_", species_name, "_from_", species_location, ".png"), 
+           plot = year_plot, width = 60, height = 30, units = "cm", dpi = 900)
+  }
+}
+
+# executing iteration for the long file!
+plot <- Map(Location_histograms, long$Specieslist, long$name)
 
