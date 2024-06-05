8587476d098aae9b5720e4fb8daca78b335a7e23
Iris van paemel <iris.van.paemel@student.howest.be>
2024-06-05 18:13:18 +0200

    searched for a mistake in this script, copy pasted it in new script 3.3.2_errorsolve_calculation.R to adjust calculations and find alternatives to shortestPath, distm and lengthline

--- new_workflow_Iris/3.3_Calculation_sea_distances.R
+++ new_workflow_Iris/3.3_Calculation_sea_distances.R
@@ -26,7 +26,7 @@ library("FRK")
 # Set working directory to directory where the R-script is saved
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Read a species list
-df <- read.csv("Output/next20_Species_Location.csv")
+df <- read.csv("theoretical_data/OccurrenceData_toKoster/alien_species.csv")
 # Read coordinates file
 Coordinates <- read.csv("Inputs/Coordinates.csv")
 
@@ -93,7 +93,8 @@ fetch_data_in_batches <- function(species_name, basisOfRecord, batch_size = 1000
     res <- occ_data(scientificName = species_name, 
                     hasCoordinate = TRUE, 
                     limit = batch_size,
-                    basisOfRecord = basisOfRecord)
+                    basisOfRecord = basisOfRecord,
+                    continent = "europe")
   combined_data <- rbind(combined_data, res$data)
   Sys.sleep(1)  # Adding a small delay to be polite to the server
   
@@ -116,8 +117,6 @@ for (species_name in unique(long$Specieslist)){
     fetch_data_in_batches(species_name, "Human observation"),
     fetch_data_in_batches(species_name, "Material sample"),
     fetch_data_in_batches(species_name, "Material citation"),
-    fetch_data_in_batches(species_name, "Preserved specimen"),
-    fetch_data_in_batches(species_name, "Fossil specimen"),
     fetch_data_in_batches(species_name, "Living specimen"),
     fetch_data_in_batches(species_name, "Occurrence")
   )
@@ -186,8 +185,8 @@ for (species_name in unique(long$Specieslist)){
 # REVISION: CALCULATE DISTANCES
 ############################################################################################
 # practice species
-#species_name <- "Acartia bifilosa"
-#species_location <- "Koster"
+species_name <- "Acartia bifilosa"
+species_location <- "Koster"
 
 # Iterate over species_name and location_name
 Calculation_seadistance <- function(species_name, species_location){
@@ -195,7 +194,7 @@ Calculation_seadistance <- function(species_name, species_location){
   print(paste0("species_name: ", species_name))
   print(paste0("species_location: ", species_location))
   
-  occurrence_coord <- paste0("OccurrenceData_test/", species_name, ".csv")
+  occurrence_coord <- paste0("theoretical_data/OccurrenceData_toKoster/alien_species.csv")
   OccurrenceData <- read.csv(occurrence_coord, header = TRUE)
   
   # getting coordinates from Coordinates dataframe to get longitude and latitude for ARMS location
@@ -209,48 +208,39 @@ Calculation_seadistance <- function(species_name, species_location){
   # make a dataframe out of the longitude and latitude called samplelocation
   samplelocation <- data.frame(Longitude = longitude, Latitude = latitude)
   
-  # Remove duplicate longitudes & latitudes  ==> from original function
-  #######################################################################
-  unique_file <- OccurrenceData[!duplicated(OccurrenceData[, c("Longitude", "Latitude")]), ]
-  
-  # check if unique_file has coordinates
-  if(nrow(unique_file) < 1) {
-    stop("Unique_file has no coordinates (line 139)")
+  # check if OccurrenceData has coordinates
+  if(nrow(OccurrenceData) < 1) {
+    stop("OccurrenceData has no coordinates (line 139)")
   }
   
-  # make matrices out of longitudes and latitudes of samplelocation and unique_file
+  # make matrices out of longitudes and latitudes of samplelocation and OccurrenceData
   # These are used in both flying distances and sea distances calculations
   sampleloc_matrix <- matrix(as.numeric(c(samplelocation$Longitude, samplelocation$Latitude)), ncol = 2)
-  uniquefile_matrix <- matrix(as.numeric(c(unique_file$Longitude, unique_file$Latitude)), ncol = 2)
+  uniquefile_matrix <- matrix(as.numeric(c(OccurrenceData$Longitude, OccurrenceData$Latitude)), ncol = 2)
   
   ##########################################################################
   # FLYING DISTANCE CALCULATION
   ##########################################################################
   
-  flying_distances <- as.numeric(distm(samplelocation[,c("Longitude", "Latitude")], 
-                                unique_file[,c("Longitude", "Latitude")], 
-                                fun = distVincentyEllipsoid))
-  # 'fun =' this method is used for distances on earth (ellipsoid)
+  # extract the single set of coordinates
+  point1 <- c(samplelocation$Longitude, samplelocation$Latitude)
+  print(point1)
+  # calculate distances
+  flying_distances <- apply(OccurrenceData, 1, function(row) {
+    point2 <- c(as.numeric(row["Longitude"]), as.numeric(row['Latitude']))
+    distVincentySphere(point1, point2)
+  })
   
   # check if directory and/or csv files already exists with flying distances (files for each species/location)
-  flydistance_file <- paste0("test_outputs/fly_distances/", species_name, "_fly_distancesTo_", species_location)
-  content_file2 <- cbind(flying_distances, unique_file$year, unique_file$month, unique_file$country)
+  flydistance_file <- paste0("theoretical_data/", species_name, "_fly_distancesTo_", species_location)
+  content_file2 <- cbind(flying_distances, OccurrenceData$year, OccurrenceData$month, OccurrenceData$country)
   colnames(content_file2) <- c("x", "year", "month", "country")
   
-  if(!dir.exists("test_outputs/fly_distances")){
-    dir.create("test_outputs/fly_distances")
-    
-    if(!file.exists(flydistance_file)){
-      write.table(content_file2, file = flydistance_file)
-    } else {
-      warning(paste0(species_name, " and ", species_location, "flying distances file already written."))
-    }
+  if(!dir.exists("theoretical_data/")){
+    dir.create("theoretical_data/")
+    write.table(content_file2, file = flydistance_file)
   } else {
-    if(!file.exists(flydistance_file)){
-      write.table(content_file2,file = flydistance_file)
-    } else {
-      warning(paste0(species_name, " and ", species_location, "flying distances file already written."))
-    }
+    write.table(content_file2,file = flydistance_file)
   }
   
   ##########################################################################
@@ -367,27 +357,58 @@ Calculation_seadistance <- function(species_name, species_location){
   }) # trycatch() closed
   
   # check if directory and/or csv files already exists with sea distances(files for each species/location)
-  distance_file <- paste0("test_outputs/sea_distances/", species_name, "_distancesTo_", species_location)
-  content_file <- cbind(sea_distances, unique_file$year, unique_file$month, unique_file$country)
+  distance_file <- paste0("theoretical_data/sea_distances/", species_name, "_distancesTo_", species_location)
+  content_file <- cbind(sea_distances, OccurrenceData$year, OccurrenceData$month, OccurrenceData$country)
   colnames(content_file) <- c("x", "year", "month", "country")
   
-  if(!dir.exists("test_outputs/sea_distances")){
-    dir.create("test_outputs/sea_distances")
+  if(!dir.exists("theoretical_data/sea_distances")){
+    dir.create("theoretical_data/sea_distances")
     write.table(content_file, file = distance_file)
     colnames(content_file)
   } else {
     write.table(content_file, file = distance_file)
   }
   
-  return(sea_distances)
+  ###########################################################
+  # CALCULATE SHORTEST SEA DISTANCE
+  ###########################################################
+  
+  shortest_fly_distance <- min(flying_distances, na.rm = TRUE)
+  print(shortest_fly_distance)
+  shortest_sea_distance <- min(sea_distances, na.rm = TRUE)
+  print(shortest_sea_distance)
+  
+  # make a list of all shortest distances per species/location
+  content_shortest <- data.frame(species_name = species_name,
+                                 species_location = species_location,
+                                 shortest_sea_distance = shortest_sea_distance,
+                                 shortest_fly_distance = shortest_fly_distance)
+  
+  shortest_distances <- append(shortest_distances, content_shortest)
+  return(shortest_distances)
+
 }
 
 error_messages <- c()
 
+# start a list to save shortest distances in
+colnames <- c("species_name", "species_location", "shortest_sea_distance", "shortest_fly_distance")
+shortest_distances <- data.frame(matrix(ncol = length(colnames), nrow = 0))
+colnames(shortest_distances) <- colnames
+
 result <- Map(Calculation_seadistance, long$Specieslist, long$name)
+shortest_distances <- result
 
 # write error file
 if (length(error_messages) > 0) {
   error_df <- data.frame(error_message = unlist(error_messages))
   write.csv(error_df, "test_outputs/species_error.csv", row.names = FALSE)
 }
+
+# write shortest distances file
+write.table(shortest_distances, file = "test_outputs/shortest_distances.csv")
+
+
+
+
+
